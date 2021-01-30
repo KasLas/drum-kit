3d33c4ca802759cc912bb6c49894909ee8396392
kasparslasinskis <kaspars.lasinskis@azeron.eu>
2021-01-30 14:38:37 +0200

    added new sound object

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Add click event listener for all .drum class buttons
-
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", handleClick);
 }
 
 function handleClick() {
-    alert("I have been clicked");
+    var drum1 = new Audio("sounds/tom-1.mp3");
+    drum1.play();
 }
